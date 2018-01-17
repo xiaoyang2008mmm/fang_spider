bfa773195ae01e5a83b39246a685b7228ad5cd3f
root <root@101-200-216-100.(none)>
2018-01-17 11:07:42 +0800

    v3

--- a.py
+++ a.py
@@ -26,28 +26,76 @@ while 1:
         c = soup1.find('a').get('href')
 	detail_url = c + '/house/'+ str(data_id) +'/housedetail.htm' 
 	print '#########'
-	print b, detail_url
+	print '小区名字:',b
+	print '详情页面',detail_url
 	#获取详情页的关键字	
 	res = requests.get(detail_url)
 	res.encoding = 'gb18030' 
 	soup2=BeautifulSoup(res.text)
 	#价格
 	h_price = soup2.find('div',{'class':'main-info-price'},text=None)
-	print h_price.getText()
+	print '价格',h_price.getText()
 	#楼盘地址
 	floor_add = soup2.findAll('li',{'class':'list-text'},text=None)
-	print (floor_add[1]).getText()
+	print '楼盘地址',(floor_add[1]).getText()
 	#开发商
 	dev_business = floor_add[0].getText()
-	print dev_business
+	print '开发商',dev_business
 	#项目特色
 	programme = soup2.findAll('span',{'class':'tag'},text=None)
-	for i in programme: print (i.getText())
+	for i in programme: print '项目特色',(i.getText())
 	#产权年限
 	owner = soup2.find('div',{'class':'clearfix cqnx_512'},text=None)
-	print owner.getText()
+	print '产权年限',owner.getText()
 	#开盘时间
+	sale_data = soup2.findAll('ul',{'class':'list clearfix'},text=None)
+	a = (sale_data[1].findAll('div',{'class':'list-right'},text=None))[2::]
+	start_time = a[0].getText() 
+	print '开盘时间',start_time
+	#交房时间
+	rev_house_time = a[1].getText()
+	print '交房时间',rev_house_time
+	#售楼地址
+	sale_building = a[2].getText()
+	print '售楼地址',sale_building
+	#主力户型
+	mainunit = (sale_data[1].findAll('div',{'class':'list-right-text'},text=None))[0].getText() 
+	print '主力户型',mainunit
 
+	######获取小区信息
+	xiaoqu_info = soup2.findAll('ul',{'class':'clearfix list'},text=None)
+	infos = xiaoqu_info[0].findAll('div',{'class':'list-right'},text=None)
+	#物业公司
+	wygs = infos[-2].getText()
+	print '物业公司',wygs
+	#建筑面积
+	jsmj = infos[1].getText()
+	print '建筑面积',jsmj
+	#占地面积
+	zdmj = infos[0].getText()
+	print '占地面积',zdmj
+	#物业费
+	wyf = infos[-1].getText()
+	print '物业费',wyf
+	#绿化率
+	lhl = infos[3].getText()
+	print '绿化率',lhl
+	#容积率
+	rjl = infos[2].getText()
+	print '容积率',rjl
+	#总户数
+	zhs = infos[-3].getText()	
+	print '总户数',zhs
+	#车位情况
+	cwqk = infos[4].getText()	
+	print '车位情况',cwqk
+	#楼盘介绍
+	lpjs = (soup2.find('p',{'class':'intro'},text=None)).getText() 
+	print '楼盘介绍',lpjs
+	#配套
+	pt = (soup2.find('ul',{'class':'sheshi_zb'},text=None)).getText()
+	print '配套',pt
+	
 
 	
 
